8087672c33fed01da82b51d43f89a2f2c5a0a9ae
Muhammad Javed Iqbal <142082823+muhammadjavediqbal@users.noreply.github.com>
2025-03-08 23:31:30 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 # main.py
 from fastapi import FastAPI, HTTPException, Request
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 import google.generativeai as genai
 import os
 
 app = FastAPI()
-
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=origins, 
+    allow_credentials=True, 
+    allow_methods=["*"], 
+    allow_headers=["*"]
+    )
 # Configure Gemini API
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")  # Set your API key as an environment variable
 if not GOOGLE_API_KEY:
